8fcc5eaae2451af6bfbb103538b63e3ac31888fc
scalarvector1003 <happysmile911003@gmail.com>
2023-02-23 14:52:37 +0900

    login API store function and transfer

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,9 @@
 const config = {
   api: {
-    SIGN_IN: "http://localhost/casino/index.php/api/user/signin",
+    SIGN_IN: "http://127.0.0.1:8000/api/users/login",
     BANK_LIST: "http://127.0.0.1:8000/api/deposit/getBank",
-    DEPOSIT : "http://127.0.0.1:8000/api/deposit/addMoney"
+    DEPOSIT : "http://127.0.0.1:8000/api/deposit/addMoney",
+    GET_SYSCONFIG: "http://127.0.0.1:8000/api/transfer/getSysConfig"
   },
 };
 
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -9,12 +9,7 @@ export const useAuthStore = defineStore({
   id:"auth",
   state:() =>({
     token:'',
-    user:{
-      id: '',
-      username:'',
-      money:0,
-      credit:0
-    } as IUser,
+    user:{} as any,
   }),
   getters:{
     getToken: (state) => state.token,
@@ -24,19 +19,19 @@ export const useAuthStore = defineStore({
     setToken(token:string){
       this.token = token;
     },
-    setUser(user:IUser){
+    setUser(user:any){
       this.user = user;
     },
     async signIn(username:string, password:string) {
       try {
         let url = config.api.SIGN_IN;
         let data = {
-          username:username,
+          UserName:username,
           password:password
         };
         const response = (await axios.post(url, data)).data;
         this.setToken(response.data.token as string);
-        this.setUser(response.data.user as IUser)
+        this.setUser(response.data as any)
         return response;
       } catch (e) {
         return e;
